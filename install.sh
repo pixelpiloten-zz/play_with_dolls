5f4943a7c5fde95fa5ad5ed9bc237cd8fef0ac61
Pixelpiloten <pixelpiloten@gmail.com>
2016-02-07 22:23:21 +0100

    Changed share-folder to a folder in user home-folder.

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+WEBDEV_SHARE=/Users/$USER/WebDev
+if [ ! -d "$WEBDEV_SHARE" ]; then
+  mkdir /Users/$USER/WebDev
+fi
 brew update
 brew install dlite
-sudo dlite install --share=/srv/www
+sudo dlite install --share=$WEBDEV_SHARE
 dlite start
 brew install docker
 brew install docker-compose
